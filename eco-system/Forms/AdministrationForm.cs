c29b049d03843edf66fbf3c8046eb5c5e1b330d3
BadDa3mon <pasha.padero@gmail.com>
2022-01-03 01:42:51 +0300

    Fix bugs

--- eco-system/Forms/AdministrationForm.cs
+++ eco-system/Forms/AdministrationForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,11 +14,19 @@ namespace eco_system.Forms
 {
     public partial class AdministrationForm : Form
     {
+        private ApiRequests mApiRequests;
         public AdministrationForm()
         {
             InitializeComponent();
 
-            Person[] persons = Task.Run(() => new ApiRequests().GetUsers()).Result;
+            mApiRequests = new ApiRequests();
+
+            updatePersonList();
+        }
+
+        private void updatePersonList()
+        {
+            Person[] persons = Task.Run(() => mApiRequests.GetUsers()).Result;
 
             if (persons != null && persons.Length > 0)
             {
@@ -25,9 +34,34 @@ namespace eco_system.Forms
                 {
                     string[] row = { person.id.ToString(), person.firstname, person.lastname, person.patronymic, person.phone, person.date_of_birth, person.address, person.place_of_work };
 
-                    people_grib_view.Rows.Add(row);
+                    people_grid_view.Rows.Add(row);
                 }
             }
+        }
+
+        private void update_button_Click(object sender, EventArgs e)
+        {
+            people_grid_view.Rows.Clear();
+
+            updatePersonList();
+        }
+
+        private void edit_person_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delete_person_button_Click(object sender, EventArgs e)
+        {
+            int rowIndex = people_grid_view.SelectedCells[0].RowIndex;
+            object personId = people_grid_view.SelectedRows[rowIndex].Cells[0];
+
+            Debug.WriteLine($"Выбранный ряд = {rowIndex}, id пользователя = {personId}");
+        }
+
+        private void add_person_button_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
